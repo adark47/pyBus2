ac64459b7d71e6e2e0e1f49403acffd852952b39
andreyteterin <adark47@gmail.com>
2018-07-12 15:59:15 +0500

    update

--- root/pyBus/lib/pyBus_module_display.py
+++ root/pyBus/lib/pyBus_module_display.py
@@ -18,24 +18,24 @@ WRITER = None  # Writer thread
 
 MENU_LEVEL = False
 
-TextT0 = None
-TextT1 = None
-TextT2 = None
-TextT3 = None
-TextT4 = None
-TextT5 = None
-TextT6 = None
+textT0 = None
+textT1 = None
+textT2 = None
+textT3 = None
+textT4 = None
+textT5 = None
+textT6 = None
 
-IndexF0 = None
-IndexF1 = None
-IndexF2 = None
-IndexF3 = None
-IndexF4 = None
-IndexF5 = None
-IndexF6 = None
-IndexF7 = None
-IndexF8 = None
-IndexF9 = None
+indexF0 = None
+indexF1 = None
+indexF2 = None
+indexF3 = None
+indexF4 = None
+indexF5 = None
+indexF6 = None
+indexF7 = None
+indexF8 = None
+indexF9 = None
 
 ############################################################################
 #
@@ -108,83 +108,83 @@ def immediateTextClear(string):
 # MK4 - Title ##############################################################
 
 def setNewTextT0(string):
-    global TextT0
-    TextT0 = string
+    global textT0
+    textT0 = string
 
 def setNewTextT1(string):
-    global TextT1
-    TextT1 = string
+    global textT1
+    textT1 = string
 
 def setNewTextT2(string):
-    global TextT2
-    TextT2 = string
+    global textT2
+    textT2 = string
 
 def setNewTextT3(string):
-    global TextT3
-    TextT3 = string
+    global textT3
+    textT3 = string
 
 def setNewTextT4(string):
-    global TextT4
-    TextT4 = string
+    global textT4
+    textT4 = string
 
 def setNewTextT5(string):
-    global TextT5
-    TextT5 = string
+    global textT5
+    textT5 = string
 
 def setNewTextT6(string):
-    global TextT6
-    TextT6 = string
+    global textT6
+    textT6 = string
 
 # MK4 - Index ##############################################################
 
 def setNewIndexF0(string):
-    global IndexF0
-    IndexF0 = string
+    global indexF0
+    indexF0 = string
     WRITER.writeIndexF0(string)
 
 def setNewIndexF1(string):
-    global IndexF1
-    IndexF1 = string
+    global indexF1
+    indexF1 = string
     WRITER.writeIndexF1(string)
 
 def setNewIndexF2(string):
-    global IndexF2
-    IndexF2 = string
+    global indexF2
+    indexF2 = string
     WRITER.writeIndexF2(string)
 
 def setNewIndexF3(string):
-    global IndexF3
-    IndexF3 = string
+    global indexF3
+    indexF3 = string
     WRITER.writeIndexF3(string)
 
 def setNewIndexF4(string):
-    global IndexF4
-    IndexF4 = string
+    global indexF4
+    indexF4 = string
     WRITER.writeIndexF4(string)
 
 def setNewIndexF5(string):
-    global IndexF5
-    IndexF5 = string
+    global indexF5
+    indexF5 = string
     WRITER.writeIndexF5(string)
 
 def setNewIndexF6(string):
-    global IndexF6
-    IndexF6 = string
+    global indexF6
+    indexF6 = string
     WRITER.writeIndexF6(string)
 
 def setNewIndexF7(string):
-    global IndexF7
-    IndexF7 = string
+    global indexF7
+    indexF7 = string
     WRITER.writeIndexF7(string)
 
 def setNewIndexF8(string):
-    global IndexF8
-    IndexF8 = string
+    global indexF8
+    indexF8 = string
     WRITER.writeIndexF8(string)
 
 def setNewIndexF9(string):
-    global IndexF9
-    IndexF9 = string
+    global indexF9
+    indexF9 = string
     WRITER.writeIndexF9(string)
 
 def refreshIndexMK4():
@@ -331,58 +331,58 @@ class busWriter(threading.Thread):
 
 # MK4 - Title ######################################################################
 
-            if TextT0 != None:
-                busWriter.writeTitleT0(self, TextT0)
+            if textT0 != None:
+                busWriter.writeTitleT0(self, textT0)
 
-            if TextT1 != None:
-                busWriter.writeTitleT1(self, TextT1)
+            if textT1 != None:
+                busWriter.writeTitleT1(self, textT1)
 
-            if TextT2 != None:
-                busWriter.writeTitleT2(self, TextT2)
+            if textT2 != None:
+                busWriter.writeTitleT2(self, textT2)
 
-            if TextT3 != None:
-                busWriter.writeTitleT3(self, TextT3)
+            if textT3 != None:
+                busWriter.writeTitleT3(self, textT3)
 
-            if TextT4 != None:
-                busWriter.writeTitleT4(self, TextT4)
+            if textT4 != None:
+                busWriter.writeTitleT4(self, textT4)
 
-            if TextT5 != None:
-                busWriter.writeTitleT5(self, TextT5)
+            if textT5 != None:
+                busWriter.writeTitleT5(self, textT5)
 
-            if TextT6 != None:
-                busWriter.writeTitleT6(self, TextT6)
+            if textT6 != None:
+                busWriter.writeTitleT6(self, textT6)
 
 # MK4 - Index ######################################################################
 
 #            if IndexF0 != None:
-#                busWriter.writeIndexF0(self, IndexF0)
+#                busWriter.writeIndexF0(self, indexF0)
 
 #            if IndexF1 != None:
-#                busWriter.writeIndexF1(self, IndexF1)
+#                busWriter.writeIndexF1(self, indexF1)
 
 #            if IndexF2 != None:
-#                busWriter.writeIndexF2(self, IndexF2)
+#                busWriter.writeIndexF2(self, indexF2)
 
 #            if IndexF3 != None:
-#                busWriter.writeIndexF3(self, IndexF3)
+#                busWriter.writeIndexF3(self, indexF3)
 
 #            if IndexF4 != None:
-#                busWriter.writeIndexF4(self, IndexF4)
+#                busWriter.writeIndexF4(self, indexF4)
 
 #            if IndexF5 != None:
-#                busWriter.writeIndexF5(self, IndexF5)
+#                busWriter.writeIndexF5(self, indexF5)
 
 #            if IndexF6 != None:
-#                busWriter.writeIndexF6(self, IndexF6)
+#                busWriter.writeIndexF6(self, indexF6)
 
 #            if IndexF7 != None:
-#                busWriter.writeIndexF7(self, IndexF7)
+#                busWriter.writeIndexF7(self, indexF7)
 
 #            if IndexF8 != None:
-#                busWriter.writeIndexF8(self, IndexF8)
+#                busWriter.writeIndexF8(self, indexF8)
 
 #            if IndexF9 != None:
-#                busWriter.writeIndexF9(self, IndexF9)
+#                busWriter.writeIndexF9(self, indexF9)
 
 
     def stop(self):
